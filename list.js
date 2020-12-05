8eb5caedcbe2a6bab42c1a3d600f04a10388a872
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-05 17:01:48 +0530

    Update list.js

--- list.js
+++ list.js
@@ -1,18 +1,21 @@
 function load(){
-    var req,data,content,txt="";
-    req = new XMLHttpRequest();
-    req.onreadystatechange = function(){
-        if(this.readyState==4&&this.status==200){
-            content=JSON.parse(this.responseText);
-            data=content.list;
-            for(i=0;i<data.length;i++){
-                txt+="<tr>"
-                txt+="<td>"+data[i]+"</td>"
-                txt+="</tr>"
-            }
-            document.getElementById("tab").innerHTML=txt;
+var req,data,content,txt=[];
+req = new XMLHttpRequest();
+req.onreadystatechange = function(){
+    if(this.readyState==4&&this.status==200){
+        content=JSON.parse(this.responseText);
+        data=content.list;
+        for(i=0;i<data.length;i++){
+            txt+="<tr>"
+            txt+="<td>"+data[i].userId+"</td>"
+            txt+="<td>"+data[i].id+"</td>"
+            txt+="<td>"+data[i].title+"</td>"
+            txt+="<td>"+data[i].completed+"</td>"
+            txt+="</tr>"
         }
-    };
-    req.open("GET","https://jsonplaceholder.typicode.com/todos",true);
-    req.send();
+        document.getElementById("tab").innerHTML=txt;
     }
+};
+req.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+req.send();
+}
