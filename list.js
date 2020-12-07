5da6bcc160be19f827edb767e18f2dc98a72b9f7
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-07 15:26:33 +0530

    Update list.js

--- list.js
+++ list.js
@@ -1,12 +1,13 @@
 function load(){
-var req,data,content,txt="";
+var req,content,txt="";
 req = new XMLHttpRequest();
 req.onreadystatechange = function(){
     if(this.readyState==4&&this.status==200) {
         content=JSON.parse(this.responseText);
         for(var i=0;i<content.length;i++){
+
             txt+="<tr>"
-            txt+="<td>"+content[i].title+"</td>"
+            txt+="<td>"+ `<input type="checkbox" onclick="counter(this)" disable style="";>`+ content[i].title+"</td>"
             txt+="</tr>"
         }
         document.getElementById("tab").innerHTML=txt;
@@ -17,4 +18,21 @@ req.send();
 }
 function logout(){
     window.location.replace("todo.html");
+}
+var count=0;
+
+function counter(value){
+    var set = new Promise(function(resolve, reject){
+        if (value.checked == true){
+            count++;
+            if (count >= 6){
+                resolve("5 tasks over");
+            }
+        }else{
+            count--;
+        }
+    });
+    set.then(function(){
+        alert("5 tasks completed already")
+    });
 }
