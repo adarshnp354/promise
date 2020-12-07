b44eb64e01aee06250eeb75f08ae461e17a4f098
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-07 12:57:56 +0530

    Update list.js

--- list.js
+++ list.js
@@ -2,15 +2,11 @@ function load(){
 var req,data,content,txt="";
 req = new XMLHttpRequest();
 req.onreadystatechange = function(){
-    if(this.readyState==4&&this.status==200){
+    if(this.readyState==4&&this.status==200) {
         content=JSON.parse(this.responseText);
-        data=content.list;
-        for(i=0;i<data.length;i++){
+        for(var i=0;i<content.length;i++){
             txt+="<tr>"
-            txt+="<td>"+data[i].userId+"</td>"
-            txt+="<td>"+data[i].id+"</td>"
-            txt+="<td>"+data[i].title+"</td>"
-            txt+="<td>"+data[i].completed+"</td>"
+            txt+="<td>"+content[i].title+"</td>"
             txt+="</tr>"
         }
         document.getElementById("tab").innerHTML=txt;
