7efda47c6ce89f93746dfeb592ac2415a94dde2a
adarsh354 <fullstack_trainee_001@thefuturalabs.com>
2020-12-09 13:51:12 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,14 +2,12 @@
 function log(){
     var usr = document.getElementById("user").value;
     var pas = document.getElementById("pass").value;
-    if(usr=="admin" && pas=="12345"){
-        alert("SUCCESS");
-        call();
-    }else{
-        alert("INVALID USERNAME OR PASSWORD");
+    function valid(usr, pas, callback) {
+        ((usr === "admin") && (pas === "12345"))? callback():alert("INVALID USERNAME OR PASSWORD");
     }
-}
+    (valid(usr, pas, call))?true:false;
+ }
 function call(){
+    alert("SUCESS")
     window.location.replace("list.html");
-    callback();
 }
